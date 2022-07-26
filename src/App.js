7b0c5f7d340880f44d5590c6b6435c5ba72a89f6
Montini <devmontini@gmail.com>
2022-07-26 19:22:10 -0300

    boostrap add

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-import "./App.css";
 import { useSelector, useDispatch } from "react-redux";
 import { getText } from "./features/reverseText";
+import { Button, Card, Form } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
 function App() {
   const [input, setInput] = useState("");
@@ -16,44 +18,49 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <>
-        <h1>Copywrite APP - Montini Franco</h1>
-        <form>
-          <input
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            required="required"
-            placeholder="Enter Text"
-          ></input>
-          <button onClick={(e) => handeleReverseText(e)} role="presentation">
-            Send
-          </button>
-        </form>
-      </>
-      <>
-        <h2>Reverse Texts</h2>
-        <ul>
-          {textReverse.status === "success" ? (
-            textReverse.text.map((el) => {
-              return (
-                <li key={Math.random() * 33 + Date.now()}>
-                  <p>{el.text}</p>
-                  {el.palindrome ? (
-                    <p>"Palindrome!"</p>
-                  ) : (
-                    <p>"Not palindrome!"</p>
-                  )}
-                </li>
-              );
-            })
-          ) : (
-            <li>
-              <p>No texts</p>
-            </li>
-          )}
-        </ul>
-      </>
+    <div className="App bg-secondary">
+      <header className="App-header">
+        <Form className="m-3">
+          <Form.Group>
+            <Form.Label className="m-3">Copywrite Code Challenge</Form.Label>
+            <Form.Control
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+              required="required"
+              placeholder="Enter Text"
+            ></Form.Control>
+            <Button onClick={(e) => handeleReverseText(e)} role="presentation">
+              Send
+            </Button>
+          </Form.Group>
+        </Form>
+        <Card className="m-3">
+          <Card.Body>
+            <Card.Title className="mb-6">List</Card.Title>
+            {textReverse.status === "success" ? (
+              textReverse.text.map((el) => {
+                return (
+                  <Card.Subtitle
+                    className="m-4"
+                    key={Math.random() * 33 + Date.now()}
+                  >
+                    {el.text}
+                    {el.palindrome ? (
+                      <Card.Text className="text-muted">Palindrome!</Card.Text>
+                    ) : (
+                      <Card.Text className="text-muted">
+                        Not palindrome!
+                      </Card.Text>
+                    )}
+                  </Card.Subtitle>
+                );
+              })
+            ) : (
+              <Card.Subtitle className="m-4 text-muted">No texts</Card.Subtitle>
+            )}
+          </Card.Body>
+        </Card>
+      </header>
     </div>
   );
 }
